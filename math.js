e4f747610eda33cc537c70c1512f66f43f5fe02b
Juan David Castro <me@juandc.co>
2022-05-25 13:56:05 -0500

    altura triangulo

--- math.js
+++ math.js
@@ -36,6 +36,18 @@ function calcularTriangulo(lado1, lado2, base, altura) {
   };
 }
 
+
+function calcularAlturaTriangulo(lado1, base) {
+  if (lado1 == base) {
+    console.warn('Este no es un triángulo isosceles');
+  } else {
+    // h = raizcuadrada(lado1**2 - (b**2)/4)
+    return Math.sqrt( (lado1 ** 2) - ( (base ** 2) ) / 4 );
+  }
+}
+
+
+
 console.log({
   ladoTriangulo1,
   ladoTriangulo2,
