23120adb8cec1f178b5a7ce60e45e1c06138d1e8
Juan David Castro <me@juandc.co>
2022-05-25 12:49:00 -0500

    circulo y math

--- math.js
+++ math.js
@@ -46,3 +46,32 @@ console.log({
 });
 
 console.groupEnd('Triangulo') 
+
+console.group('Circle')
+
+const radioCirculo = 3;
+const diametroCirculo = radioCirculo * 2;
+const PI = 3.1415;
+
+const circunferencia = diametroCirculo * PI;
+const areaCirculo = (radioCirculo ** 2) * PI;
+
+console.log({
+  radioCirculo,
+  diametroCirculo,
+  PI,
+  circunferencia,
+  areaCirculo,
+});
+
+function calcularCirculo(radio) {
+  const diametro = radio * 2;
+  const radioAlCuadrado = Math.pow(radio, 2);
+  
+  return {
+    circunferencia: diametro * Math.PI,
+    area: radioAlCuadrado * Math.PI,
+  };
+}
+
+console.groupEnd('Circle')
