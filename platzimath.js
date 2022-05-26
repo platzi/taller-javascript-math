f03a6132602219b80deba0f205a44c4a539986c9
Juan David Castro <me@juandc.co>
2022-05-26 16:41:14 -0500

    moda

--- platzimath.js
+++ platzimath.js
@@ -5,6 +5,28 @@ function esImpar(lista) {
   return lista.length % 2;
 }
 
+function calcularModa(lista) {
+  const listaCount = {};
+
+  for (let i = 0; i < lista.length; i++) {
+    const elemento = lista[i];
+
+    if (listaCount[elemento]) {
+      listaCount[elemento] += 1;
+    } else {
+      listaCount[elemento] = 1;
+    }
+  }
+
+  const listaArray = Object.entries(listaCount);
+  const listaOrdenada = ordenarListaBidimensional(listaArray, 1)
+  const listaMaxNumber = listaOrdenada[listaOrdenada.length - 1];
+  const moda = listaMaxNumber[0];
+  // console.log({listaCount, listaArray, listaOrdenada, listaMaxNumber});
+  // console.log('La moda es: ' + listaMaxNumber[0]);
+  return moda;
+}
+
 function calcularMediana(listaDesordenada) {
   const lista = ordenarLista(listaDesordenada);
   const listaEsPar = esPar(lista);
@@ -40,23 +62,21 @@ function calcularPromedio(lista) {
 
 function ordenarLista(listaDesordenada) {
   function ordenarListaSort(valorAcumulado, nuevoValor) {
-    // if (valorAcumulado > nuevoValor) {
-    //   return 1;
-    // } else if (valorAcumulado == nuevoValor) {
-    //   return 0;
-    // } else if (valorAcumulado < nuevoValor) {
-    //   return -1;
-    // }
-
     return valorAcumulado - nuevoValor;
-    // return nuevoValor - valorAcumulado;
-    // return 5 - 10 -> -5;
-    // return 5 - 5 -> 0;
-    // return 10 - 5 -> 5;
   }
   
-  // const lista = listaDesordenada.sort(ordenarListaSort);
-  const lista = listaDesordenada.sort((a,b) => a-b);
+  // const lista = listaDesordenada.sort((a,b) => a-b);
+  const lista = listaDesordenada.sort(ordenarListaSort);
   
+  return lista;
+}
+
+// [ [0,1],  [0,1],  [0,1] ]
+function ordenarListaBidimensional(listaDesordenada, i) {
+  function ordenarListaSort(valorAcumulado, nuevoValor) {
+    return valorAcumulado[i] - nuevoValor[i];
+  }
+  
+  const lista = listaDesordenada.sort(ordenarListaSort);
   return lista;
 }
