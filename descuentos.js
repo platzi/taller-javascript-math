0df8cebfe6a0cebcd60a62aea315693c0f863205
Juan David Castro <me@juandc.co>
2022-05-26 14:13:29 -0500

    find filter

--- descuentos.js
+++ descuentos.js
@@ -5,7 +5,27 @@ const pResult = document.querySelector('#result');
 
 btn.addEventListener('click', calcularPrecioConDescuento);
 
-const arrayUObjecto = undefined; // ['cupones': descuento] {}?
+// const arrayUObjecto = undefined; // ['cupones': descuento] {}?
+
+// const couponsObj = {
+//   'JuanDC_es_Batman': 30,
+//   '3456789': 25,
+//   '123': 15,
+// };
+
+const couponsList = [];
+couponsList.push({
+  name: 'JuanDC_es_Batman',
+  discount: 30,
+});
+couponsList.push({
+  name: 'pero_es_un_secreto',
+  discount: 25,
+});
+couponsList.push({
+  name: 'no_le_digas_a_nadie',
+  discount: 15,
+});
 
 function calcularPrecioConDescuento() {
   const price = Number(inputPrice.value);
@@ -18,17 +38,44 @@ function calcularPrecioConDescuento() {
   
   let discount;
 
-  switch (coupon) {
-    case 'JuanDC_es_Batman':
-      discount = 30;
-      break;
-    case 'no_le_digas_a_nadie':
-      discount = 25;
-      break;
-    default:
-      pResult.innerText = 'El cupón no es válido';
-      return;
+  function isCouponInArray(couponElement) {// {name, discount}
+    return couponElement.name == coupon;
   }
+  
+  const couponInArray = couponsList.find(isCouponInArray); // {}
+
+  if (couponInArray) {
+    discount = couponInArray.discount;
+  } else {
+    pResult.innerText = 'El cupón no es válido';
+    return;
+  }
+
+  console.log({
+    coupon, 
+    discount,
+    couponInArray,
+    couponsList,
+  });
+  
+  // if (couponsObj[coupon]) {
+  //   discount = couponsObj[coupon];
+  // } else {
+    // pResult.innerText = 'El cupón no es válido';
+    // return;
+  // }
+  
+  // switch (coupon) {
+  //   case 'JuanDC_es_Batman':
+  //     discount = 30;
+  //     break;
+  //   case 'no_le_digas_a_nadie':
+  //     discount = 25;
+  //     break;
+  //   default:
+      // pResult.innerText = 'El cupón no es válido';
+      // return;
+  // }
   
   // if (coupon == 'JuanDC_es_Batman') {
   //   discount = 30;
