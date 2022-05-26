31a78b05cdc2d5bd42a8d9bacd89a7645a7ff50d
Juan David Castro <me@juandc.co>
2022-05-26 15:35:04 -0500

    mediana par

--- platzimath.js
+++ platzimath.js
@@ -9,7 +9,22 @@ function calcularMediana(lista) {
   const listaEsPar = esPar(lista);
 
   if (listaEsPar) {
-    // ...
+    const indexMitad1ListaPar = (lista.length / 2) - 1;
+    const indexMitad2ListaPar = lista.length / 2;
+    // const mitad1ListaPar = lista[(lista.length / 2) - 1];
+    // const mitad2ListaPar = lista[lista.length / 2];
+
+    // lista[indexMitad1ListaPar] 
+    // lista[indexMitad2ListaPar] 
+    // [lista[indexMitad1ListaPar], lista[indexMitad2ListaPar]]
+    // const listaMitades = [mitad1ListaPar, mitad2ListaPar];
+
+    const listaMitades = [];
+    listaMitades.push(lista[indexMitad1ListaPar]);
+    listaMitades.push(lista[indexMitad2ListaPar]);
+
+    const medianaListaPar = calcularPromedio(listaMitades);
+    return medianaListaPar;
   } else {
     const indexMitadListaImpar = Math.floor(lista.length / 2);
     const medianaListaImpar = lista[indexMitadListaImpar];
@@ -20,12 +35,12 @@ function calcularMediana(lista) {
 }
 
 function calcularPromedio(lista) {
-    function sumarTodosElementos(valorAcumulado, nuevoValor) {
+  function sumarTodosElementos(valorAcumulado, nuevoValor) {
     return valorAcumulado + nuevoValor;
   }
 
   const sumaLista = lista.reduce(sumarTodosElementos);  
   const promedio = sumaLista / lista.length;
-  console.log(promedio);
+  // console.log(promedio);
   return promedio;
 }
