a0699bb4c5edcb82a399651397c76d2c9c1e0ede
Juan David Castro <me@juandc.co>
2022-05-26 15:23:32 -0500

    mediana impar

--- platzimath.js
+++ platzimath.js
@@ -1,22 +1,30 @@
-// [1,2,3,4]
-function calcularPromedio(lista) {
-  // let sumaLista = 0;
-  // for (let i = 0; i < lista.length; i++) {
-  //   sumaLista = sumaLista + lista[i];
-  // }
+function esPar(lista) {
+  return !(lista.length % 2);
+}
+function esImpar(lista) {
+  return lista.length % 2;
+}
 
-  function sumarTodosElementos(valorAcumulado, nuevoValor) {
+function calcularMediana(lista) {
+  const listaEsPar = esPar(lista);
+
+  if (listaEsPar) {
+    // ...
+  } else {
+    const indexMitadListaImpar = Math.floor(lista.length / 2);
+    const medianaListaImpar = lista[indexMitadListaImpar];
+    console.log(indexMitadListaImpar);
+    console.log(medianaListaImpar);
+    return medianaListaImpar;
+  }
+}
+
+function calcularPromedio(lista) {
+    function sumarTodosElementos(valorAcumulado, nuevoValor) {
     return valorAcumulado + nuevoValor;
   }
-  
-  // const ejemplo = (a, b) => a + b;
-  
-  // const sumarTodosElementos =
-  //   (valorAcumulado, nuevoValor) => valorAcumulado + nuevoValor;
-  // const sumaLista = lista.reduce((a, b) => a + b);
 
-  const sumaLista = lista.reduce(sumarTodosElementos);
-
+  const sumaLista = lista.reduce(sumarTodosElementos);  
   const promedio = sumaLista / lista.length;
   console.log(promedio);
   return promedio;
