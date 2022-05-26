8e0b4e37d288110392770dff18dc2b8ac0df5010
Juan David Castro <me@juandc.co>
2022-05-26 10:48:31 -0500

    cupones easy

--- descuentos.js
+++ descuentos.js
@@ -1,27 +1,44 @@
 const inputPrice = document.querySelector('#price');
-const inputDiscount = document.querySelector('#discount');
+const inputCoupon = document.querySelector('#coupon');
 const btn = document.querySelector('#calcular');
 const pResult = document.querySelector('#result');
 
 btn.addEventListener('click', calcularPrecioConDescuento);
 
+const arrayUObjecto = undefined; // ['cupones': descuento] {}?
+
 function calcularPrecioConDescuento() {
-  
   const price = Number(inputPrice.value);
-  const discount = Number(inputDiscount.value);
-  
-  console.log({ price, discount });
-  
-  if (!price || !discount) {
+  const coupon = inputCoupon.value;
+
+  if (!price || !coupon) {
     pResult.innerText = 'CHANCLA por favor llena el formulario';
     return;
   }
+  
+  let discount;
 
-  if (discount > 100) {
-    pResult.innerText = 'Ajá, ya quisieras, no te vamos dar plata, PAGA!';
-    return;
+  switch (coupon) {
+    case 'JuanDC_es_Batman':
+      discount = 30;
+      break;
+    case 'no_le_digas_a_nadie':
+      discount = 25;
+      break;
+    default:
+      pResult.innerText = 'El cupón no es válido';
+      return;
   }
-
+  
+  // if (coupon == 'JuanDC_es_Batman') {
+  //   discount = 30;
+  // } else if (coupon == 'no_le_digas_a_nadie') {
+  //   discount = 25;
+  // } else {
+  // pResult.innerText = 'El cupón no es válido';
+  // return;
+  // }
+  
   const newPrice = (price * (100 - discount)) / 100;
 
   pResult.innerText = 'El nuevo precio con descuento es $' + newPrice;
