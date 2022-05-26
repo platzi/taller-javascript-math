f6f404fede1ddf98f8f13719d5d2f9d403fc21ad
Juan David Castro <me@juandc.co>
2022-05-26 15:00:09 -0500

    reduce

--- platzimath.js
+++ platzimath.js
@@ -1,9 +1,21 @@
 // [1,2,3,4]
 function calcularPromedio(lista) {
-  let sumaLista = 0;
-  for (let i = 0; i < lista.length; i++) {
-    sumaLista = sumaLista + lista[i];
+  // let sumaLista = 0;
+  // for (let i = 0; i < lista.length; i++) {
+  //   sumaLista = sumaLista + lista[i];
+  // }
+
+  function sumarTodosElementos(valorAcumulado, nuevoValor) {
+    return valorAcumulado + nuevoValor;
   }
+  
+  // const ejemplo = (a, b) => a + b;
+  
+  // const sumarTodosElementos =
+  //   (valorAcumulado, nuevoValor) => valorAcumulado + nuevoValor;
+  // const sumaLista = lista.reduce((a, b) => a + b);
+
+  const sumaLista = lista.reduce(sumarTodosElementos);
 
   const promedio = sumaLista / lista.length;
   console.log(promedio);
