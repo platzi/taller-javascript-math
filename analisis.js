0edd6ca2bdb750af91caee0c992a3a455882b7ed
Juan David Castro <me@juandc.co>
2022-05-27 15:08:41 -0500

    mediana por empresa

--- analisis.js
+++ analisis.js
@@ -46,3 +46,46 @@ function proyeccionPorPersona(nombrePersona) {
 
   return nuevoSalario;
 }
+
+// Análisis empresarial
+/* {
+  Industrias Mokepon: {
+    2018: [salario]
+  }
+  Industrias Mokepon: {
+    2018: [salario, salarios, salrios]
+    2019: 
+    2025: 
+    2026: 
+  },
+  Industrias Mokepon: {},
+  Industrias Mokepon: {},
+  Industrias Mokepon: {},
+} */
+const empresas = {};
+
+for (persona of salarios) {
+  for (trabajo of persona.trabajos) {
+    if (!empresas[trabajo.empresa]) {
+      empresas[trabajo.empresa] = {};
+    }
+
+    if (!empresas[trabajo.empresa][trabajo.year]) {
+      empresas[trabajo.empresa][trabajo.year] = [];
+    }
+
+    empresas[trabajo.empresa][trabajo.year].push(trabajo.salario);
+  }
+}
+
+console.log({empresas});
+
+function medianaEmpresaYear(nombre, year) {
+  if (!empresas[nombre]) {
+    console.warn('La empresa no existe');
+  } else if (!empresas[nombre][year]) {
+    console.warn('La empresa no dio salarios ese año');
+  } else {
+    return PlatziMath.calcularMediana(empresas[nombre][year]);
+  }
+}
